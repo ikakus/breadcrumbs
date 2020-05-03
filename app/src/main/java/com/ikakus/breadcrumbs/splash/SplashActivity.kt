685fe8188b298367de5f6166ff10890230269b17
ikakus <idadiani88@gmail.com>
2020-05-03 09:53:12 +0300

    add separate splash activity

---
+++ app/src/main/java/com/ikakus/breadcrumbs/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.ikakus.breadcrumbs.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import com.ikakus.breadcrumbs.core.base.BaseActivity
+import com.ikakus.breadcrumbs.home.MainActivity
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        scheduleSplashScreen()
+    }
+
+    private fun scheduleSplashScreen() {
+        val splashScreenDuration = 1000L
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+            },
+            splashScreenDuration
+        )
+    }
+}
